70f597daf1d368ef683cfdab1a49159efd03fd13
vivekreddy911 <140322855+vivekreddy911@users.noreply.github.com>
2023-12-17 13:11:48 +0530

    Add files via upload

---
+++ prac.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    q.push(1);
+    q.push(5);
+    q.push(3);
+    q.push(6);
+    q.push(2);
+    q.push(0);
+
+    int sum = 0;
+    while (!q.empty()) {
+        sum += q.front(); 
+        q.pop();         
+    }
+    cout << "Sum of all elements in the queue: " << sum <<endl;
+    return 0;
+}
